c694699f8dd80bdc83ef2bbe91e6d9643ff6da2f
sunwenzhe-git <13836705550@163.com>
2024-07-18 11:09:43 +0800

    feat: 🎸 delete umi

--- umi-plugin-qiankun/examples/master/app.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'dva/fetch';
-window.__POWERED_BY_QIANKUN_PARENT__ = true;
-export const qiankun = request('/apps').then(apps => ({
-  apps,
-  fetch: url => {
-    console.log('静态资源fetch覆盖');
-    return fetch(url);
-  },
-}));
--- umi-plugin-qiankun/src/slave/runtimePlugin.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function rootContainer(container: HTMLElement) {
-  const value = (window as any).g_rootExports;
-  // eslint-disable-next-line global-require
-  const { Context } = require('@tmp/qiankunContext');
-  return React.createElement(Context.Provider, { value }, container);
-}
