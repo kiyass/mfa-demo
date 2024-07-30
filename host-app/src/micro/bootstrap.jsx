9fefdc54df6a9130bea42b20d0859fc834355c1e
sunwenzhe-git <13836705550@163.com>
2024-07-30 16:38:50 +0800

    test: 💍 store

--- host-app/src/micro/bootstrap.jsx
+++ host-app/src/micro/bootstrap.jsx
@@ -1,5 +1,6 @@
 import { renderApp } from "./App";
 import microApp from "@micro-zoe/micro-app";
+import "./store";
 
 microApp.start({
   "router-mode": "native", // 所有子应用都设置为native模式
---
+++ host-app/src/micro/store.jsx
@@ -0,0 +1,41 @@
+/*
+ *  Author:
+ *  Description:
+ */
+import { GlobalStore } from "redux-micro-frontend";
+
+let initialState = {
+  global: 0,
+  local: 0,
+  todo: 0,
+};
+
+const CounterReducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT_GLOBAL")
+    return { ...state, global: state.global + 1 };
+  if (action.type === "DECREMENT_GLOBAL")
+    return { ...state, global: state.global - 1 };
+
+  if (action.type === "INCREMENT_LOCAL")
+    return { ...state, local: state.local + 1 };
+  if (action.type === "DECREMENT_LOCAL")
+    return { ...state, local: state.local - 1 };
+
+  if (action.type === "ADD_TODO") return { ...state, todo: state.todo + 1 };
+  if (action.type === "REMOVE_TODO") return { ...state, todo: state.todo - 1 };
+
+  return state;
+};
+
+const store = GlobalStore.Get(false);
+
+store.CreateStore("CounterApp", CounterReducer, []);
+
+store.RegisterGlobalActions("CounterApp", [
+  "INCREMENT_GLOBAL",
+  "DECREMENT_GLOBAL",
+  "ADD_TODO",
+  "REMOVE_TODO",
+]);
+
+export default store;
