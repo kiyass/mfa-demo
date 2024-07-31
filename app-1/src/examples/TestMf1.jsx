c4bf96d4a727280b2e485001dd914aafcebe43cd
XS367 <xuanjiaqi@harmonycloud.cn>
2024-07-31 10:16:10 +0800

    refactor: micro-app mf

--- app-1/src/examples/TestMf1.jsx
+++ app-1/src/examples/TestMf1.jsx
@@ -2,22 +2,22 @@
  *  Author:
  *  Description:
  */
+import Mf2 from "mf2/Mf2";
 import React from "react";
-// import Mf2 from "mf2/Mf2";
 // import TestEcharts from "appx/TestEcharts";
-// import { getLib1InstanceId } from "mf3/utils";
+import { getLib1InstanceId } from "mf3/utils";
 
 export default () => {
   return (
     <div style={{ margin: 100 }}>
       <button
         onClick={() => {
-          // console.log("getLib1InstanceId", getLib1InstanceId());
+          console.log("getLib1InstanceId", getLib1InstanceId());
         }}
       >
         xxx
       </button>
-      {/* <Mf2 /> */}
+      <Mf2 />
       {/* <Mf1 /> */}
 
       {/* <TestEcharts /> */}
