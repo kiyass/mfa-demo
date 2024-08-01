c874301a1e9dbd7f6366688c56a252384904049d
sunwenzhe-git <13836705550@163.com>
2024-08-01 16:01:46 +0800

    feat: 🎸 qiankun-utils

--- app-2-1/src/index.jsx
+++ app-2-1/src/index.jsx
@@ -1,3 +1,5 @@
-import "qiankun-utils/publicPath";
+import { setPublicPath } from "qiankun-utils/publicPath";
+const publicPath = setPublicPath();
+if (publicPath) __webpack_public_path__ = publicPath;
 const { bootstrap, mount, unmount } = await import("./bootstrap");
 export { bootstrap, mount, unmount };
--- app-3/src/index.jsx
+++ app-3/src/index.jsx
@@ -1,4 +1,6 @@
-import "qiankun-utils/publicPath";
+import { setPublicPath } from "qiankun-utils/publicPath";
+const publicPath = setPublicPath();
+if (publicPath) __webpack_public_path__ = publicPath;
 const { bootstrap, mount, unmount } = await import("./bootstrap");
 
 export { bootstrap, mount, unmount };
