0116ab52f5f3eaa9deb68797a43e77111f6603f0
sunwenzhe-git <13836705550@163.com>
2024-08-14 09:38:42 +0800

    test: 💍 editor

---
+++ app-1-1/src/examples/TestEditor.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+
+import Editor from "@monaco-editor/react";
+
+export default function TestEditor() {
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    alert(editorRef.current.getValue());
+  }
+
+  return (
+    <>
+      <button onClick={showValue}>Show value</button>
+      <Editor
+        height="90vh"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        onMount={handleEditorDidMount}
+      />
+    </>
+  );
+}
